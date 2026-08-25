08d7b3b987838f37f73f43127802041d3248bb6b
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-08-26 02:01:55 +0700

    Corrected main form

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace Blue_Arrow
 {
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Blue_Arrow
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GUI_Form());
         }
     }
 }
