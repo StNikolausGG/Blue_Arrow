191fe838f9d2c70f82b00f15f423f5ea82820bea
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-08-25 15:35:45 +0700

    Added Json lib

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Blue_Arrow
 {
-    public partial class Form1 : Form
+    public partial class GUI_Form : Form
     {
-        public Form1()
+        public GUI_Form()
         {
             InitializeComponent();
         }
